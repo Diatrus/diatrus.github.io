36495a46c4092e4592fc844eb734cfb89515041f
Diatrus <pinseeker75@outlook.com>
2019-09-07 23:01:40 -0400

    this didn't commit apparently...

--- nito/updaterepo.sh
+++ nito/updaterepo.sh
@@ -8,4 +8,10 @@ apt-ftparchive packages ./debs > Packages
 gzip -c9 Packages > Packages.gz
 xz -c9 Packages > Packages.xz
 zstd -c19 Packages > Packages.zst
+bzip2 -c9 Packages > Packages.bz2
+
+apt-ftparchive release -c ./configs/nito.conf . > Release
+
+gpg -abs --default-key A62E24F587D7497DA677036D2948E440D73080B4 -o Release.gpg Release
+
 echo "Done updating!"

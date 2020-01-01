36ab786a36b8bfaefc77851107d89441cc778601
Diatrus <pnseeker75@outlook.com>
2020-01-01 15:16:34 -0500

    This is fucked

--- nito/updaterepo.sh
+++ nito/updaterepo.sh
@@ -2,7 +2,7 @@
 script_full_path=$(dirname "$0")
 cd $script_full_path
 
-rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release.gpg Release
+rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release
 
 apt-ftparchive packages ./debs > Packages
 gzip -c9 Packages > Packages.gz
@@ -11,7 +11,5 @@ zstd -c19 Packages > Packages.zst
 bzip2 -c9 Packages > Packages.bz2
 
 apt-ftparchive release -c ./configs/nito.conf . > Release
-
-gpg -abs --default-key A62E24F587D7497DA677036D2948E440D73080B4 -o Release.gpg Release
 
 echo "Done updating!"

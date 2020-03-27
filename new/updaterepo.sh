42015dd9049b702330646081b53717e1525091b3
Diatrus <pinseeker75@outlook.com>
2020-03-26 20:44:17 -0400

    .

--- new/updaterepo.sh
+++ new/updaterepo.sh
@@ -6,6 +6,8 @@ rm Packages Packages.zst Release
 
 apt-ftparchive packages ./debs > Packages
 zstd -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
 
 apt-ftparchive release -c ./configs/new.conf . > Release
 

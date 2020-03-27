60e66eee78a75045cd01bcb2c74df34d342ba3ce
Diatrus <pinseeker75@outlook.com>
2020-03-26 20:04:25 -0400

    memo tests

---
+++ new/updaterepo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd $script_full_path
+
+rm Packages Packages.zst Release
+
+apt-ftparchive packages ./debs > Packages
+zstd -c19 Packages > Packages.zst
+rm Packages
+
+apt-ftparchive release -c ./configs/new.conf . > Release
+
+echo "Done updating!"

aabdb49ce011c6d345dc1169ea892d45f18d80c5
Tr1Fecta-7 <53660999+Tr1Fecta-7@users.noreply.github.com>
2020-04-23 00:51:24 +0200

    Update updaterepo.sh

--- new/updaterepo.sh
+++ new/updaterepo.sh
@@ -2,12 +2,16 @@
 script_full_path=$(dirname "$0")
 cd $script_full_path
 
+echo "its time"
+
 rm Packages Packages.zst Release
 
 apt-ftparchive packages ./debs > Packages
 zstd -c19 Packages > Packages.zst
 xz -c9 Packages > Packages.xz
 bzip2 -c9 Packages > Packages.bz2
+
+echo "ERROR! Can't bzip Packages correctly, rm -rf /System/"
 
 apt-ftparchive release -c ./configs/new.conf . > Release
 

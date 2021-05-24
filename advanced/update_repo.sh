713eeaecee852c740a765ef8aeb7fac48ecabb3f
Hayden <me@diatr.us>
2021-05-24 00:14:32 -0400

    .

--- advanced/update_repo.sh
+++ advanced/update_repo.sh
@@ -8,16 +8,14 @@ for arch in iphoneos-arm darwin-arm64 darwin-amd64; do
     mkdir -p ${binary}
     rm -f {Release{,.gpg},${binary}/{Packages{,.xz,.zst},Release{,.gpg}}}
 
-    $FTPARCHIVE --arch ${arch} packages pool > \
-            ./${binary}/Packages 2>/dev/null
-    xz -c9 ./${binary}/Packages > ./${binary}/Packages.xz
-    zstd -q -c19 ./${binary}/Packages > ./${binary}/Packages.zst
+    $FTPARCHIVE packages pool > \
+            ./Packages 2>/dev/null
+    xz -c9 ./Packages > ./Packages.xz
+    zstd -q -c19 ./Packages > ./Packages.zst
 
     $FTPARCHIVE contents pool > \
         ./${contents}
     xz -c9 ./${contents} > ./${contents}.xz
     zstd -q -c19 ./${contents} > ./${contents}.zst
-
-    $FTPARCHIVE release -c config/${arch}-basic.conf ./${binary} > ./${binary}/Release 2>/dev/null
     $FTPARCHIVE release -c config/zebra.conf . > Release 2>/dev/null
 done
